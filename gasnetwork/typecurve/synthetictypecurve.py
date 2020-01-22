45b91a0530ec97eab18a7c5b6d84e168014d7584
michael@intelics.com.au <michael@intelics.com.au>
2020-01-23 09:25:23 +1000

    limear ramp

--- gasnetwork/typecurve/synthetictypecurve.py
+++ gasnetwork/typecurve/synthetictypecurve.py
@@ -1,10 +1,21 @@
 class SyntheticTypeCurve ():
 
-    def __init__(self):
-        pass
+    """
+        Implementing synth as per "Developing synthetic reservoir type curve model for use in evaluating surface facility and gathering pipe network designs" 
+
+    """
+
+    def __init__(self, q_zero, q_peak, t_peak):
+        self.q_zero = q_zero
+        self.q_peak = q_peak
+        self.t_peak = t_peak
 
     def build_curve(self):
         pass
+
+    def calculate_linear_ramp_slope(self):
+        # Equation 3
+        return (self.q_peak - self.q_zero) / self.t_peak
 
     def get_flow(self, interval):
         pass
@@ -13,4 +24,3 @@ class SyntheticTypeCurve ():
     def get_backpressure(self, interval, flow):
         pass
 
-    
--- gasnetwork/utils/engunits.py
+++ gasnetwork/utils/engunits.py
@@ -2,3 +2,16 @@
     File for converting engineering units
 """
 
+UNITS_LOOKUP = {}
+UNITS_LOOKUP['pressure'] = []
+UNITS_LOOKUP['pressure'].append({'units': 'Pa', 'gain': 1, 'bias': 0})
+UNITS_LOOKUP['pressure'].append({'units': 'kPag', 'gain': 0.001, 'bias': -101325})
+
+def convert_value(measurement, value, units):
+    # convert pressure in given units
+    ret_val = value
+    if measurement in UNITS_LOOKUP:
+        for conversion in UNITS_LOOKUP[measurement]:
+            if conversion['units'] == units:
+                ret_val = (value - conversion['bias']) * conversion['gain']
+    return ret_val
