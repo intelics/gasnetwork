adfe0b463df2fed70695cbc35d22a3156785cdf1
Ramtin Radnia <radniaramtin@gmail.com>
2020-01-23 15:33:07 +1000

    changes to tests and synthetictypecurve

--- gasnetwork/typecurve/synthetictypecurve.py
+++ gasnetwork/typecurve/synthetictypecurve.py
@@ -1,51 +1,88 @@
+import math
+
 class SyntheticTypeCurve ():
-    import math
+    
 
     """
         Implementing synth as per "Developing synthetic reservoir type curve model for use in evaluating surface facility and gathering pipe network designs" 
     """
 
-    def __init__(self, t, q_zero, q_peak, t_peak, k, b, a, t_plat):
-        self.t = t
-        self.q_zero = q_zero
+    def __init__(self, q_zero, q_peak, t_peak, t_plat, k, b, a):
+        # Time (t) - measured in months (m)
+        self.t = 0 
+        self.t_plat = max(t_plat, 0)
+
+        # Standard Volume Flow (q) - measured in Standard meters cubes per second (Sm^3/s)
+        self.q_zero = min(q_zero, q_peak)
         self.q_peak = q_peak
-        self.t_peak = t_peak
+        self.t_peak = max(t_peak, 0)
+        
+
+        # Interpolation degree between linear and sinusoidal ramp to peak (k)
         self.k = k
+        if k>1:
+            k=1
+        else:
+            if k<0:
+                k=0
+            else:
+                k=k
+
+        # Flow decline exponent (b)
         self.b = b
-        self.a = a 
-        self.t_plat = t_plat
+        if b>1:
+            b=1
+        else:
+            if b<0:
+                b=0
+            else:
+                b=b
+
+        # Flow decline constant (a) --> a=ln(q_zero/q)/t
+        self.a = max(a, 0) 
+        
 
     def build_curve(self):
         pass
 
     def calculate_ramp_to_peak_flow(self):
         # Equation 2
+        # returns the q_flow at time self.f
         return (self.calculate_linear_ramp_slope * self.t) + self.q_zero 
 
     def calculate_linear_ramp_slope(self):
         # Equation 3
-        return (self.q_peak - self.q_zero) / self.t_peak
+        try:     
+            return (self.q_peak - self.q_zero) / self.t_peak
+        except Exception as err:
+            return 0
 
     def calculate_sine_ramp_flow(self):
         # Equation 4
+        try:
         return ((self.q_peak / 2) * math.sin((math.pi / self.t_peak) * self.t - (math.pi / 2))) + (self.q_peak / 2)
+        except Exception as err:
+            return 0
 
     def calculate_ramp_flow(self):
         # Equation 5
         return (self.k * self.calculate_sine_ramp_flow) + ((1 - self.k) * self.calculate_ramp_to_peak_flow) 
     
-    def calculate_peak_flow_plateau(self).
+    def calculate_peak_flow_plateau(self):
         # Equation 6
         return (self.q_peak)
     
     def calculate_decline_flow(self):
+        try:
         if b != 0:
             # Equation 7
             return self.q_peak / math.pow((1 + (self.b * self.a * (self.t - self.t_peak - self.t_plat))) , (1/self.b))
         else:
             # Equation 8
             return self.q_peak * math.exp(-1 * (self.a) * (self.t - self.t_peak - self.t_plat))    
-
+        except Exception as err:
+            return 0
+            
     def calculate_synthetic_type_curve(self):
         # Equation 9
         if self.t == 0:
--- tests/synthetictypecurves/synthetictypecurves.py
+++ tests/synthetictypecurves/synthetictypecurves.py
@@ -1,5 +1,78 @@
-from gasnetwork.typecurve.synthetictypecurve import SyntheticTypeCurve
-
+from .typecurve.synthetictypecurve import SyntheticTypeCurve
 import unittest
 
 # https://docs.python.org/3/library/unittest.html
+
+class TestMethods(unittest.TestCase):
+    def test_calculate_ramp_to_peak_flow(self):
+        # Errors considered: q_zero>q_peak, t_peak==0
+        type_curve = SyntheticTypeCurve(0, 100, 5, 6, 0, 1, 1)
+        # q_zero, q_peak, t_peak, t_plat, k, b, a
+
+        # t = 0, q_zero = 0, k = 0 (full linear ramp), expect ramp_to_peak_flow to return 0
+        q = type_curve.calculate_ramp_to_peak_flow()
+        self.assertEqual(q, 0)
+
+        # t = 1, q_zero = 0, k = 0 (full linear ramp), expect ramp_to_peak_flow to return linear_ramp_slope
+        type_curve.t = 1
+        q = type_curve.calculate_ramp_to_peak_flow()
+        self.assertEqual(q, type_curve.calculate_linear_ramp_slope)
+
+    def test_calculate_sine_ramp_flow(self):
+        # Errors considered: q_zero>q_peak, t_peak==0
+        type_curve = SyntheticTypeCurve(0, 100, 5, 6, 0, 1, 1)
+        
+        
+        # q_peak = 100, t_peak = 5, t=0, expect sine_ramp_flow to return 0
+        type_curve.t = 0
+        q = type_curve.calculate_sine_ramp_flow()
+        self.assertEqual(q,0)
+
+        # q_peak = 100, t_peak = 5, t=1, expect sine_ramp_flow to return 9.549150281
+        type_curve.t = 1
+        q = type_curve.calculate_sine_ramp_flow()
+        self.assertEqual(q,9.549150281)
+
+    def test_calculate_ramp_flow(self):
+        # Errors considered: k>1, k<0
+        type_curve = SyntheticTypeCurve(0, 100, 5, 6, 0, 1, 1)
+
+        # k = 0 (full linear ramp), expect ramp_flow to return ramp_to_peak_flow
+        type_curve.t = 0
+        q = type_curve.calculate_ramp_flow()
+        self.assertEqual(q, type_curve.calculate_ramp_to_peak_flow)
+
+        # k = 1 (full sinusodal ramp), expect ramp_flow to return sin_ramp_flow
+        type_curve = SyntheticTypeCurve(0, 100, 5, 6, 1, 1, 1)
+        q = type_curve.calculate_ramp_flow()
+        self.assertEqual(q, type_curve.calculate_sine_ramp_flow)
+
+    def test_calculate_peak_flow_plateau(self):
+        type_curve = SyntheticTypeCurve(0, 100, 5, 6, 0, 1, 1)
+
+        # q_peak = 100, expect peak_flow_plateau to return 100
+        q_type_curve.calculate_peak_flow_plateau()
+        self.assertEqual(q, 100)
+
+    def test_calculate_decline_flow(self):
+        # Errors considered: t_plat < 0, t_peak < 0, a<0, b>1, b<0, math.pow((1 + (self.b * self.a * (self.t - self.t_peak - self.t_plat))) , (1/self.b)) == 0
+        type_curve = SyntheticTypeCurve(0, 100, 5, 6, 0, 1, 1)
+
+        # q_peak = 100, b = 1, a = 1, t = 0, t_peak = 5, t_plat = 6, expect decline flow to return -10
+        type_curve.t = 0
+        q_type_curve.calculate_decline_flow()
+        self.assertEqual(q, -10)
+
+        type_curve = SyntheticTypeCurve(0, 100, 5, 6, 0, 0, 1)
+
+        # q_peak = 100, b = 1, a = 1, t = 1, t_peak = 5, t_plat = 6, expect decline flow to return 2202646.579
+        type_curve.t = 1
+        q_type_curve.calculate_decline_flow()
+        self.assertEqual(q, 2202646.579)
+
+        
+
+        
+
+
+
